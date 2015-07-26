99d7238dbce6d7622e3c4f7771fb61e157e49389
demonbre <demonbre@gmail.com>
2015-07-26 11:26:17 +0200

    prva iteracija
    Change-Id: I85bdf568f451a34983e579a3e567759aa5a2be11

---
+++ kernelmako.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+############################################################
+# BUILD THE ROM                                            #
+############################################################
+
+cd ~/du51-aosp && make clobber
+
+# Turn on compiler caching
+
+export USE_CCACHE=1
+
+prebuilts/misc/linux-x86/ccache/ccache -M 85G
+
+export DU_BUILD_TYPE=TEST
+
+# export USE_PREBUILT_CHROMIUM=1
+
+# Start build process
+
+cd ~/du51-aosp && . build/envsetup.sh && lunch du_mako-userdebug
+
+time make -j16 bootimage
+
+############################################################
+# NOTIFY                                                   #
+############################################################
+
+echo -e "\n"
+
+echo 'Build completed successfully and copied over to /DU-Official.'
+
+echo -e "\n"
+
+# Notify user of success
+
+beep -f 784 -r 3 -l 100
+sleep .1
+beep -f 784 -l 600
+beep -f 622 -l 600
+beep -f 698 -l 600
+beep -f 784 -l 200
+sleep .2
+beep -f 698 -l 200
+beep -f 784 -l 800
+
+exit
